d668637fbb138ac2b6fbb4f0b796aa6af496d601
nathalie <nathalie@email.com>
2025-10-20 14:36:50 +1300

    created view for logout, url for logout and test.

--- message/urls.py
+++ message/urls.py
@@ -7,7 +7,7 @@ from django.urls import path, include
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from message.views import send_message, LoginView
+from message.views import send_message, LoginView, LogoutView
 from message.viewsets import Chat_roomViewSet, MessageViewSet
 
 router = DefaultRouter()
@@ -19,4 +19,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('send_message/', send_message, name='send_message'),
     path('login/',LoginView.as_view(), name='login'),
+    path('logout/',LogoutView.as_view(), name='logout'),
 ]
